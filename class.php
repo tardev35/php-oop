0d621209ca0644787325f2213f559de28601aa87
tardev35 <suksanban1992@gmail.com>
2023-11-28 13:33:53 +0700

    php Class

--- class.php
+++ class.php
@@ -1,24 +1,57 @@
 <?php
 class MyClass
 {
-    public $Fname = 'Niran';
-    public function showname()
+    private $private = 'PRIVATE !';
+    protected $protected = 'PROTECTED !';
+    public $public = 'PUBLIC';
+    private $name;
+
+    // Setter
+    public function setName(string $name)
     {
-        echo $this->Fname;
+        $this->name = $name;
+    }
+
+    // Getter
+    public function getName()
+    {
+        return $this->name;
     }
 }
-class Constuct1
-{
-    public $prop = 'Learn OOP';
-    function __construct()
-    {
-        echo $this->prop;
-        $this->hello();
-    }
-    public function hello()
-    {
-        echo 'Hello , PHP';
-    }
-}
-$obj1 = new Constuct1();
-// $obj1->showname();
+
+$obj1 = new MyClass();
+$obj1->setName('NiranDev');
+echo $obj1->getName();
+
+
+
+
+
+
+// class MyClass3
+// {
+//     private $private = 'PRIVATE !';
+//     protected $protected = 'PROTECTED !';
+//     public $public = 'PUBLIC';
+
+//     function __construct()
+//     {
+//         echo self::class . ': ';
+//         echo $this->private . ' , ';
+//         echo $this->protected . ' , ';
+//         echo $this->public;
+//         echo '<br><br>';
+//     }
+// }
+// class MysubClass3 extends MyClass3
+// {
+//     function __construct()
+//     {
+//         echo self::class . ': ';
+//         echo $this->protected . ' , ';
+//         echo $this->public;
+//         echo '<br><br>';
+//     }
+// }
+// $obj = new MyClass3();
+// $obj2 = new MysubClass3();
